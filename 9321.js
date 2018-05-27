4d5a45be0e34ac514ca0b66c4c457ba907937b8b
Chow <ShChChow@gmail.com>
2018-05-27 15:21:42 +1000

    Current Front-end Version
    HTML JS and api callback, async

--- 9321.js
+++ 9321.js
@@ -1,3 +1,4 @@
+
 var map;
 var neighborhoods = [
     {lat: -33.91337, lng: 151.23171},
@@ -7,23 +8,22 @@ var neighborhoods = [
 ];
 var markers = [];
 var marker;
-var myCenter = {lat: -33.91637, lng: 151.23071};
+var myCenter = {lat: -33.91665490690587, lng: 151.2312249841309};
 
-function CenterControl(controlDiv, map, txt) {
+function MapMenu(controlDiv, map, txt) {
 
+        // tab controllers
+                
         // Set CSS for the control border.
         var controlUI = document.createElement('div');
-        controlUI.style.backgroundColor = '#fff';
-        controlUI.style.border = '2px solid #fff';
-        controlUI.style.borderRadius = '3px';
-        controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
         controlUI.style.cursor = 'pointer';
         controlUI.style.marginBottom = '22px';
         controlUI.style.marginLeft = '15px';
         controlUI.style.marginRight = '15px';
         controlUI.style.marginTop = '15px';
         controlUI.style.textAlign = 'center';
-        controlUI.title = 'Click to recenter the map';
+        controlUI.title = 'Click to change mode';
+        controlUI.setAttribute("class", 'btn btn-primary');
         controlDiv.appendChild(controlUI);
 
         // Set CSS for the control interior.
@@ -33,12 +33,11 @@ function CenterControl(controlDiv, map, txt) {
         controlText.style.fontSize = '16px';
         controlText.style.lineHeight = '38px';
         controlText.innerHTML = txt;
-        controlText.class = "btn btn-primary";
         controlUI.appendChild(controlText);
 
         // Setup the click event listeners: simply set the map to Chicago.
         controlUI.addEventListener('click', function() {
-          map.setCenter(myCenter);
+          //drop();
         });
 
       }
@@ -52,13 +51,14 @@ function drop() {
 
 function addMarkerWithTimeout(position, timeout) {
       window.setTimeout(function() {
-        markers.push(new google.maps.Marker({
+        var m = new google.maps.Marker({
             position: position,
             map: map,
             draggable:true,
             animation: google.maps.Animation.DROP
-        })
-      );
+        });
+        m.addListener('click',toggleBounce);
+        markers.push(m);
   }, timeout);
 }
 
@@ -69,36 +69,79 @@ function clearMarkers() {
   markers = [];
 }
 
+
 function toggleBounce() { 
+  var currLat = marker.getPosition().lat();
+  var currLng = marker.getPosition().lng();
+  
+  // 反向 解析
+  var retAddress;
+  var retLocation;
+  var geocoder = new google.maps.Geocoder();
+  var arr = new Array();
+  geocoder.geocode(
+    {location:new google.maps.LatLng(currLat, currLng)},
+    function geoResults(results, status){
+      //这里处理结果和上面一模一样
+      if (status == google.maps.GeocoderStatus.OK) {
+        retAddress = results[0].formatted_address
+        retLocation = results[0].geometry.location;
+        arr[0] = retAddress;
+        arr[1] = retLocation;
+        // ----- wyj ----
+        // console.log(arr[0].split(', ')[1].split('NSW ')[0]+'council');
+        // geocoder.geocode({address:arr[0].split(', ')[1].split('NSW ')[0]+'council, NSW'},function geoResults(results, status){
+          // if (status == google.maps.GeocoderStatus.OK) {
+                // alert('Analysis 1: '+results[0].formatted_address);
+                // alert('Analysis 2: '+results[0].geometry.location);
+          // }
+        // });
+        // ----- wyj ----
+        var currReverseGeo = arr;
+        console.log(currReverseGeo[0]);
+        console.log(currReverseGeo[1]);
+        var imgUrl = 'https://maps.googleapis.com/maps/api/streetview?size=400x280&location='+currLat+','+currLng+'&fov=90&heading=90&pitch=10';
+        var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">'+
+            currReverseGeo[0].split(', ')[1].split('NSW ')[0]+'</h1>'+
+            '<div id="bodyContent">'+
+            '<p><b>'+currReverseGeo[0].split(', ')[1].split('NSW ')[0]+'</b>'+
+            ' Information about live ranking, criminal and renting statistics.</p>'+
+            '<img src="'+imgUrl+'" style="width:400px; height:280px;" />'+
+            '<p>Geo Postcode: _'+ currReverseGeo[0].split('NSW ')[1].substring(0,4) + '</p>' +
+            //'GeoReturnLoca: '+ currReverseGeo[1] +
+            '<p>Ref Criminal Data: <br />'+
+            '2016: 7092 Cases.<br />'+
+            '2017: 8217 Cases.<br />'+
+            '2018: 6982 Cases.<br /></p>'+
+            '</div>'+
+            '</div>';
 
-  var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Sample Data</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Kingsford</b>'+
-      ' Sample data.</p>'+
-      '<p>Ref Criminal Data: <br />'+
-      '2016: 0 Cases.<br />'+
-      '2017: 7 Cases.<br />'+
-      '2018: 5 Cases.<br /></p>'+
-      '</div>'+
-      '</div>';
-
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-  infowindow.open(map, marker);
-  if (marker.getAnimation() !== null) {
-    //marker.setAnimation(null);
-    console.log(marker.getPosition().lat()+","+marker.getPosition().lng());
-  } else {
-    //marker.setAnimation(google.maps.Animation.BOUNCE);
-    console.log(marker.getPosition().lat()+","+marker.getPosition().lng());
-  }
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+        infowindow.open(map, marker);
+        if (marker.getAnimation() !== null) {
+          marker.setAnimation(null);
+        } else {
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+        console.log(currLat+","+currLng);
+      }else{
+        alert("found error " + status);
+      }
+    }
+  );
+  
 }
 
 function initMap() {
+    
+  
+  // load cowra map
+  var cowraTest = JSON.parse(data);
   map = new google.maps.Map(document.getElementById('map'), {
     center: myCenter,
     zoom: 16,
@@ -106,6 +149,48 @@ function initMap() {
     zoomControl: true,
     scaleControl: true
   });
+  // set cowra test
+  var colors = ['#FF0230', '#EEAE00', '#BB0328', '#8E2110', '#440000'];
+  var count = 0;
+  for(var j in cowraTest){
+      count = count+1;
+      var cowradata = cowraTest[j];
+      var cowraPosition = [];
+          for(var i=0; i<cowradata.length; i++){
+              var currPos = {};
+              currPos['lat'] = cowradata[i][1];
+              currPos['lng'] = cowradata[i][0];
+              cowraPosition.push(currPos);
+          }
+      //console.log(cowraPosition);
+      var flightPath = new google.maps.Polygon({
+        path: cowraPosition,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        fillColor: colors[count%5],
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+      });
+
+      google.maps.event.addListener(flightPath, "click", function(event) {  
+        var lat = event.latLng.lat();  
+        var lng = event.latLng.lng();  
+        // 经纬度  
+        //alert("Lat=" + lat + "; Lng=" + lng);  
+        marker.setMap(null);
+        marker = new google.maps.Marker({
+            position: {lat:lat, lng:lng},
+            map: map,
+            title: 'check info or move by click',
+            draggable: true,
+            animation: google.maps.Animation.DROP
+        });
+        marker.addListener('click', toggleBounce);
+      });  
+      flightPath.setMap(map);
+  }
+  
+  //
   marker = new google.maps.Marker({
     position: myCenter,
     map: map,
@@ -116,21 +201,37 @@ function initMap() {
   marker.addListener('click', toggleBounce);
   
   var btnDiv = document.createElement('div');
-  var btn = new CenterControl(btnDiv, map, 'check criminal');
+  var btn = new MapMenu(btnDiv, map, 'Free Press');
   btnDiv.index = 1;
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(btnDiv);
   
   
   var btnDiv1 = document.createElement('div');
-  var btn1 = new CenterControl(btnDiv1, map, 'check renting');
+  var btn1 = new MapMenu(btnDiv1, map, 'Ranking');
   btnDiv1.index = 2;
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(btnDiv1);
   
+  
   var btnDiv2 = document.createElement('div');
-  var btn2 = new CenterControl(btnDiv2, map, 'check total ranking');
+  var btn2 = new MapMenu(btnDiv2, map, 'Criminal');
   btnDiv2.index = 3;
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(btnDiv2);
   
-
+  
+  var btnDiv3 = document.createElement('div');
+  var btn3 = new MapMenu(btnDiv3, map, 'Renting');
+  btnDiv3.index = 4;
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(btnDiv3);
+  
+  
+  
+  // 左键点击获取经纬度
+  // google.maps.event.addListener(map, "click", function(event) {  
+    // var lat = event.latLng.lat();  
+    // var lng = event.latLng.lng();  
+    // // 经纬度  
+    // //alert("Lat=" + lat + "; Lng=" + lng);  
+    // addMarkerWithTimeout({lat: lat, lng: lng}, 200);
+  // });  
   
 }
